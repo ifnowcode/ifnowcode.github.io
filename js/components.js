80a82c65673929f3511f174e0427aaf3707f8045
ifnowcode <ifnowcode@gmail.com>
2026-05-25 14:37:56 -0700

    Adjusted neon sign width

--- js/components.js
+++ js/components.js
@@ -583,8 +583,8 @@ class NeonTubeSign extends Element {
 
     const svg = `
     <svg class="neon-tube-sign-svg"
-         viewBox="0 0 360 100"
-         width="360"
+         viewBox="0 0 100 100"
+         width="100"
          height="100"
          xmlns="http://www.w3.org/2000/svg">
 
